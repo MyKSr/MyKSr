4e4db8b0b9b3e85ad849925c8dc38bf8df2e9c3d
Nikki Galusha <nikkigalusha@gmail.com>
2016-10-13 15:16:42 -0700

    Update to ES6

--- server/server.js
+++ server/server.js
@@ -1,21 +1,21 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-var app = express();
+const app = express();
 //set up the port, 3000 by default
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 //allow parsing json and url
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : false}));
 
 //load static files
-app.use(express.static(__dirname + 'public'));
+app.use(express.static(`${__dirname}/../public`));
 
 //get and post requests goes here
 
 
 //start server
-app.listen(port);
-
-console.log('Listening to port  ', port);
+app.listen(port, () => {
+  console.log('Listening on...', port);
+});
