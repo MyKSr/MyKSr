68fb3b309f37a19aba6e5bce90e268b0d4d2ae6a
Seungkyu Choi <VictorChoi@dyn-160-39-182-100.dyn.columbia.edu>
2016-10-19 00:07:41 -0400

    stage to my repo

--- public/users/users.js
+++ public/users/users.js
@@ -3,11 +3,23 @@ angular.module('myksr.users', [])
 .controller('UsersCtrl', function($scope, $window, $http) {
   console.log('INSIDE USERS');
 
+  // eventually Fetch the users who are in the same group as the rater,
+  // for now, fetch all users, then save it as a scope variable
+  // 
+  
   $scope.rate = function() {
     $window.location = '#/ratings';
   }
 
-  $scope.clickFriend = function() {
-  	$http.get();
+  $scope.clickFriend = function(rater, rated) {
+  	var urlString = `/${rater}/${rated}`;
+  	$http.get(urlString).then(function (resonse) {
+  	// If !(response), redirect them to rating page
+
+  	//else direct them to the friend's info page
+
+  	}, function (error) {
+
+  	});
   }
 });
