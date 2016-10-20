f09d0d3f9b3e37473a8637e60a38d1f2cba3ad80
chris <fivewingedangel@gmail.com>
2016-10-20 11:27:53 -0700

    rating submit button no longer sends request on empty field

--- public/ratings/ratings.js
+++ public/ratings/ratings.js
@@ -2,22 +2,28 @@ angular.module('myksr.ratings', [])
 
 .controller('RatingsCtrl', function($scope, $http, $window) {
   // On click we want to render the result of rating that person INCLUDING their new average rating
-  $scope.submit = function() {
-  	$http({
-  		method: 'POST',
-  		url: '/postrating',
-      data: {rate: $scope.rate,
-             rater: 'Victor',
-             rated: 'Victor'
-      }
-  	}).then(function(res) {
-      $window.location = '#/result';
-      console.log('CLIENT POST RES ',res);
-    }, function(res) {
-      console.error(res);
-    });
+  $scope.rate = "";
 
-    $scope.rate = "";
+  $scope.submit = function() {
+    if ($scope.rate) {
+      console.log('inside rating submit');      
+    	$http({
+    		method: 'POST',
+    		url: '/postrating',
+        data: {rate: $scope.rate,
+               rater: 'Victor',
+               rated: 'Victor'
+        }
+    	}).then(function(res) {
+        $window.location = '#/result';
+        console.log('CLIENT POST RES ',res);
+      }, function(res) {
+        console.error(res);
+      });
+      
+      $scope.rate = "";
+    }
+
 
   }
 })
