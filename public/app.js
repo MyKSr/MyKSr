dffb9ecbda335da942026ca34e38eee85bca0eb5
Seungkyu Choi <VictorChoi@Seungkyus-MacBook-Pro.local>
2016-10-21 11:52:33 -0700

    sign in works

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-const myksr = angular.module('myksr', [ 'myksr.ratings', 'myksr.profile', 'myksr.result', 'myksr.services', 'myksr.users', 'myksr.signup', 'ngRoute']);
+const myksr = angular.module('myksr', [ 'myksr.ratings', 'myksr.profile', 'myksr.result', 'myksr.services', 'myksr.users', 'myksr.signup', 'myksr.signin', 'ngRoute']);
 
 myksr.config(function($routeProvider) {
 
