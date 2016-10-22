1688c82b611ec4343429f5c8fa5e270edf86bb07
Seungkyu Choi <VictorChoi@Seungkyus-MBP.att.net>
2016-10-22 11:37:18 -0700

    result page show the average value

--- public/result/result.js
+++ public/result/result.js
@@ -5,19 +5,25 @@ angular.module('myksr.result', ['myksr.services'])
   $http.get(`/getAllRatings/${information.clickedUser}`).then(function (res) {
     //This should never happen because, the user would have been directed to 
     // rating page, not result page if the user has not rated this friend
+  	$scope.averageRating = 0;
+  	$scope.allComments = [];
     if (!res.data[0]) {
     	alert('This person has not been rated yet');
     	$scope.yourRating = 0;
     	$scope.yourComment = 'You have not rated this person';
-    	$scope.averageRating = 0;
-    	$scope.allComments = [];
     } else {
+    	var count = 0;
     	for (var ratingObj of res.data) {
+    		count++;
         if (ratingObj.username === information.currentUser) {
-        	$scope.currnetUser = ratingObj.firstname;
-          ratingObj.
+        	$scope.currnetUserName = ratingObj.firstname;
+          $scope.yourRating = ratingObj.rating;
+          $scope.yourComment = ratingObj.comment;
         }
+        $scope.averageRating += ratingObj.rating;
+        $scope.allComments.push(ratingObj.comment);
     	}
+    	$scope.averageRating = $scope.averageRating/count;
     }
   });
 });
